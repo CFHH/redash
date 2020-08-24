d307eaf229d77384fe398d77860e25efa6c66fb4
CFHH <13436361@qq.com>
2020-08-24 10:56:48 +0000

    start

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 redis-cli -h localhost -p 6379 flushall
 ./manage.py runserver  --host 0.0.0.0 &
 ./manage.py rq scheduler &
-./manage.py rq worker scheduled_queries &
-./manage.py rq worker queries periodic emails default schemas &
+./manage.py rq worker queries &
+./manage.py rq worker scheduled_queries periodic emails default schemas &
